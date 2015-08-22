3fab5d2f3cb329c5ebc1226a6605e8de8084caa3
Aaron Rice <aaronrice@Aarons-MacBook-Pro.local>
2015-08-21 17:08:22 -0700

    Code cleanup.

--- src/shared/components/App/AppHandler.js
+++ src/shared/components/App/AppHandler.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
-import test from './test';
-import './app.scss';
-
-
-// var RouteHandler = Router.RouteHandler;
-
-console.log(test());
 
 export default class App extends React.Component {
   render() {
